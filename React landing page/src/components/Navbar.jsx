7521f24b980689d01d9728cdfb674dcaf8604daa
Kevin-gram <k.nyiringan@alustudent.com>
2024-07-30 16:47:09 +0200

    nav bar

--- React landing page/src/components/Navbar.jsx
+++ React landing page/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 export default function Navbar() {
   return (
-    <>
-      <p className="font-bolder text-4xl text-center">this is the nav bar</p>
-    </>
+    <div className="flex justify-evenly h-24px py-5 bg-slate-100">
+      <div className="text-center">
+        <h1 className=" font-bold  text-3xl">REACT LANDING PAGE</h1>
+      </div>
+      <div className="flex justify-between gap-7 items-center">
+        <p className="font-semibold text-lg">FEATURES </p>
+        <p className="font-semibold text-lg">ABOUT</p>
+        <p className="font-semibold text-lg">SERVICES </p>
+        <p className="font-semibold text-lg">GALLERY </p>
+        <p className="font-semibold text-lg">TESTIMONIALS </p>
+        <p className="font-semibold text-lg">TEAM </p>
+        <p className="font-semibold text-lg">CONTACT </p>
+      </div>
+    </div>
   );
 }
